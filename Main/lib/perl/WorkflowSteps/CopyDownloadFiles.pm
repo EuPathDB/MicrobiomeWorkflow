76734b0e80d3e3af75365d258f8c241be2712b91
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-02-05 11:13:05 -0500

    FTP download

--- Main/lib/perl/WorkflowSteps/CopyDownloadFiles.pm
+++ Main/lib/perl/WorkflowSteps/CopyDownloadFiles.pm
@@ -10,36 +10,31 @@ use Digest::SHA qw(sha1_hex);
 sub run {
   my ($self, $test, $undo) = @_;
 
-  my $inputFileBaseName = $self->getParamValue('inputFileBaseName');
-  my $downloadDir = $self->getParamValue('relativeDownloadSiteDir');
+  my $sourceDir = join("/", $self->getWorkflowDataDir() , $self->getParamValue('sourceDir'));
+  my $allDatasetsPath = join("/", $sourceDir, $self->getParamValue('allDatasetsFileName'));
 
-  # standard parameters for making download files
-  my $websiteFilesDir = $self->getWebsiteFilesDir($test);
-  my $workflowDataDir = $self->getWorkflowDataDir() . "/downloadSiteFiles";
+  my @datasets;
+  open(my $fh, "<", $allDatasetsPath) or die "$!: $allDatasetsPath";
+  while(<$fh>){
+     chomp;
+     s{otuDADA2_(.*)_RSRC}{$1};
+     push @datasets, $_;
+  }
+  die "No datasets in $allDatasetsPath" unless @datasets;
 
-  opendir my $dir, $workflowDataDir or die "Cannot open directory: $workflowDataDir";
-  my @files = readdir $dir;
-  closedir $dir;
+  my $targetDir = join("/", $self->getWebsiteFilesDir($test), $self->getParamValue('targetDir'));
 
   if ($undo) {
-    $self->runCmd(0, "rm -fr $websiteFilesDir/$downloadDir/*");
+    $self->runCmd(0, "rm -fr $targetDir/*");
   } else {
+    $self->runCmd($test, "printf \"IndexIgnore *\" > $targetDir/.htaccess");
 
-    $self->runCmd($test, "printf \"IndexIgnore *\" > $websiteFilesDir/$downloadDir/.htaccess");
+    for my $dataset (@datasets) {
+      my $datasetTargetDir = join("/", $targetDir, sha1_hex("otuDADA2_${dataset}_RSRC"));
 
-    for my $datasetName (@files) {
-      next if ($datasetName eq '.' || $datasetName eq '..');
-      $datasetName =~ s/($inputFileBaseName)_//g;
-      my $outFile = $datasetName;
-      $datasetName =~ s/\.txt//;
-      next if ($datasetName eq 'ontologyMetadata');
-      my $digest = sha1_hex($datasetName);
-      my $copyToDownloadDir = "$websiteFilesDir/$downloadDir/$digest";
-
-      $self->runCmd($test, "mkdir -p $copyToDownloadDir");
-      $self->runCmd($test, "printf \"IndexIgnoreReset ON\nIndexIgnore ..\" > $copyToDownloadDir/.htaccess");
-      $self->runCmd($test, "cp $workflowDataDir/${inputFileBaseName}_ontologyMetadata.txt $copyToDownloadDir/ontologyMetadata.txt");
-      $self->runCmd($test, "cp $workflowDataDir/${inputFileBaseName}_$outFile $copyToDownloadDir/$outFile");
+      $self->runCmd($test, "mkdir -pv $datasetTargetDir");
+      $self->runCmd($test, "printf \"IndexIgnoreReset ON\nIndexIgnore ..\" > $datasetTargetDir/.htaccess");
+      $self->runCmd($test, "cp -v $sourceDir/${dataset}* $datasetTargetDir/");
     }
   }
 }
--- Main/lib/perl/WorkflowSteps/MakeDownloadFiles.pm
+++ Main/lib/perl/WorkflowSteps/MakeDownloadFiles.pm
@@ -3,54 +3,118 @@ package MicrobiomeWorkflow::Main::WorkflowSteps::MakeDownloadFiles;
 @ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
 
 use strict;
+use warnings;
 use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+use ApiCommonData::Load::MBioResultsTable;
 
+
+use GUS::ObjRelP::DbiDatabase;
+use DBI;
 sub run {
   my ($self, $test, $undo) = @_;
 
-  my $outputFileBaseName = $self->getParamValue('outputFileBaseName');
+  
+  my $ampliconTaxaSuffix = $self->getParamValue('ampliconTaxaSuffix');
+  my $wgsTaxaSuffix = $self->getParamValue('wgsTaxaSuffix');
+  my $level4ECsSuffix = $self->getParamValue('level4ECsSuffix');
+  my $pathwayAbundancesSuffix = $self->getParamValue('pathwayAbundancesSuffix');
+  my $pathwayCoveragesSuffix = $self->getParamValue('pathwayCoveragesSuffix');
 
-  my $sql = "
-select * from apidbtuning.taxonrelativeabundance ra,
-apidbtuning.samples s,
-apidbtuning.panextdbrls pedb
-where ra.name = s.name
-and ra.protocol_app_node_id = pedb.pan_id
-and pedb.dataset_name != 'ISASimple_RSRC'
-";
+  my $outputDir = join("/", $self->getWorkflowDataDir() , $self->getParamValue('outputDir'));
 
-  my $ontologyMetadataSql = "
-select distinct o.ontology_term_source_id as iri
-      , o.ontology_term_name as label
-      , o.type as type
-      , o.parent_ontology_term_name as parentLabel
-      , m.category as category
-      , o.description as definition
-from apidbtuning.Ontology o 
-left join apidbtuning.Metadata m 
-on o.ontology_term_source_id = m.property_source_id 
-where o.ontology_term_source_id is not null
-";
+  my $allResultsDir = join("/", $self->getWorkflowDataDir(), $self->getParamValue('allResultsDir'));
 
-  #TODO owl reader, like done for clinepi
-
-  my $workflowDataDir = $self->getWorkflowDataDir() . "/downloadSiteFiles";
-  my $outFile = "$workflowDataDir/${outputFileBaseName}_masterDataTable.txt";
-  my $ontologyFile = "$workflowDataDir/ontologyMetadata.txt";
+  my $outFileDatasets = join("/", $outputDir, $self->getParamValue('allDatasetsFileName'));
+  my $outFileSampleDetails = join("/", $outputDir, "all_sample_details.tsv");
 
   if ($undo) {
-      $self->runCmd(0, "rm -f $outFile");
-      $self->runCmd(0, "rm -f $ontologyFile");
+    $self->runCmd(0, "rm -f $outFileDatasets");
+    $self->runCmd(0, "rm -f $outFileSampleDetails");
   } else {
-      if ($test) {
-	    $self->runCmd(0,"echo test > $outFile");
+    if ($test) {
+      $self->runCmd(0,"echo test > $outFileDatasets");
+      $self->runCmd(0,"echo test > $outFileSampleDetails");
+      return;
+    }
+    my $datasets = $self->writeDatasetsAndReadThemBack($test, $outFileDatasets); 
+    my $sampleDetailsByDataset = $self->writeSampleDetailsAndReadThemBack($test, $outFileSampleDetails); 
+    for my $dataset (@{$datasets}){
+      my $ampliconTaxaPath = "$allResultsDir/${dataset}${ampliconTaxaSuffix}";
+      my $wgsTaxaPath = "$allResultsDir/${dataset}${wgsTaxaSuffix}";
+      my $level4ECsPath = "$allResultsDir/${dataset}${level4ECsSuffix}";
+      my $pathwayAbundancesPath = "$allResultsDir/${dataset}${pathwayAbundancesSuffix}";
+      my $pathwayCoveragesPath = "$allResultsDir/${dataset}${pathwayCoveragesSuffix}";
+      my ($ampliconTaxaTable, $wgsTaxaTable, $level4ECsTable, $pathwaysTable);
+      if (-f $ampliconTaxaPath){
+        $ampliconTaxaTable = ApiCommonData::Load::MBioResultsTable->ampliconTaxa($ampliconTaxaPath);
+        $ampliconTaxaTable->addSampleDetails($sampleDetailsByDataset->{$dataset});
+        $ampliconTaxaTable->writeTabSampleDetails("$outputDir/$dataset.16s_DADA2.sample_details.tsv");
+        $ampliconTaxaTable->writeBiom("$outputDir/$dataset.16s_DADA2.taxon_abundance.biom");
+        $ampliconTaxaTable->writeTabData("$outputDir/$dataset.16s_DADA2.taxon_abundance.tsv");
       }
-      $self->runCmd($test,"makeFileWithSql --outFile $outFile --sql \"$sql\" --verbose --includeHeader --outDelimiter '\\t'");
-      $self->runCmd($test,"makeFileWithSql --outFile $ontologyFile --sql \"$ontologyMetadataSql\" --verbose --includeHeader --outDelimiter '\\t'");
+      if (-f $wgsTaxaPath){
+        $wgsTaxaTable = ApiCommonData::Load::MBioResultsTable->wgsTaxa($wgsTaxaPath);
+        $level4ECsTable = ApiCommonData::Load::MBioResultsTable->wgsFunctions("level4EC", $level4ECsPath);
+        $pathwaysTable = ApiCommonData::Load::MBioResultsTable->wgsPathways($pathwayAbundancesPath, $pathwayCoveragesPath);
 
-      my $cmd = "Rscript $ENV{GUS_HOME}/bin/splitMbioDownloadSiteFiles.R $workflowDataDir $outputFileBaseName"; 
-      $self->runCmd($test, $cmd);
+        $wgsTaxaTable->addSampleDetails($sampleDetailsByDataset->{$dataset});
+        $level4ECsTable->addSampleDetails($sampleDetailsByDataset->{$dataset});
+        $pathwaysTable->addSampleDetails($sampleDetailsByDataset->{$dataset});
+        $wgsTaxaTable->writeTabSampleDetails("$outputDir/$dataset.WGS.sample_details.tsv");
+        $wgsTaxaTable->writeBiom("$outputDir/$dataset.WGS.taxon_abundance.biom");
+        $level4ECsTable->writeBiom("$outputDir/$dataset.WGS.level4ECs.biom");
+        $pathwaysTable->writeBiom("$outputDir/$dataset.WGS.pathways.biom");
+        $wgsTaxaTable->writeTabData("$outputDir/$dataset.WGS.taxon_abundance.tsv");
+        $level4ECsTable->writeTabData("$outputDir/$dataset.WGS.level4ECs.tsv");
+        $pathwaysTable->writeTabData("$outputDir/$dataset.WGS.pathways.tsv");
+
+      }
+      if (! -f $ampliconTaxaPath && ! -f $wgsTaxaPath){
+        die "Error: no files for $dataset (neither $ampliconTaxaPath nor $ampliconTaxaPath )";
+      } 
+    }
   }
 }
 
+sub writeDatasetsAndReadThemBack {
+  my ($self, $test, $outFileDatasets) = @_;
+  my $sqlDatasets = "select distinct DATASET_NAME from apidbtuning.panextdbrls where DATASET_NAME != 'ISASimple_RSRC'";
+  $self->runCmd($test,"makeFileWithSql --outFile $outFileDatasets --sql \"$sqlDatasets\" --verbose --outDelimiter '\\t'");
+
+  my @result;
+  open(my $fh, "<", $outFileDatasets) or die "$!:$outFileDatasets";
+  while(<$fh>){
+   chomp;
+   s{otuDADA2_(.*)_RSRC}{$1};
+   push @result, $_;
+  }
+  return \@result;
+}
+
+sub writeSampleDetailsAndReadThemBack {
+  my ($self, $test, $outFileSampleDetails) = @_;
+  my $sqlSampleDetails = <<"EOF";
+select m.dataset_name
+      , m.pan_name as sample_id
+      , o.ontology_term_name as property_name
+      , m.string_value as property_value
+from apidbtuning.Ontology o
+left join apidbtuning.Metadata m
+on o.ontology_term_source_id = m.property_source_id
+where o.ontology_term_source_id is not null
+EOF
+  
+  $self->runCmd($test,"makeFileWithSql --outFile $outFileSampleDetails --sql \"$sqlSampleDetails\" --verbose --outDelimiter '\\t'");
+  my %result;
+  open(my $fh, "<", $outFileSampleDetails) or die "$!:$outFileSampleDetails";
+  while(<$fh>){
+    chomp;
+    my ($datasetName, $sampleId, $propertyName, $propertyValue) = split "\t";
+    $datasetName =~ s{otuDADA2_(.*)_RSRC}{$1};
+    $sampleId =~ s{ .*$}{};
+
+    $result{$datasetName}{$sampleId}{$propertyName} = $propertyValue;
+  }
+  return \%result;
+}
 1;

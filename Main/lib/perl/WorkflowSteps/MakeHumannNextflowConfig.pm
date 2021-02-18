8c7b06b40cf7bfab4f019934064dfbd66b207f93
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-02-18 15:35:10 -0500

    Update nextflow config maker to one I used for the Resistome study

--- Main/lib/perl/WorkflowSteps/MakeHumannNextflowConfig.pm
+++ Main/lib/perl/WorkflowSteps/MakeHumannNextflowConfig.pm
@@ -25,28 +25,41 @@ sub run {
 
     open(F, ">", $configPath) or die "$! :Can't open config file '$configPath' for writing";
 
+### reasons for settings, by Wojtek
+# ref dbs in home dir: the tools are installed to my home dir too
+# retrying downloads: really useful when downloading from the public ENA FTP
+# kneaddata memory - it runs bowtie2 against hg37, which I saw to take 3.2GB peak
+# humann memory - it runs diamond which promises to take about six times --block-size in GB
     print F
 "params {
   sampleToFastqsPath = '$clusterSampleToFastqPath' 
   resultDir = '$clusterResultDir'
-  kneaddataCommand = \"kneaddata --trimmomatic ~/lib/Trimmomatic-0.39 --bypass-trf --reference-db ~/kneaddata_databases\"
+  kneaddataCommand = \"kneaddata --trimmomatic ~/lib/Trimmomatic-0.39 --max-memory 3000m --bypass-trf --reference-db ~/kneaddata_databases\"
   wgetCommand = \"wget --waitretry=10 --read-timeout=20 --retry-connrefused --tries 3\"
+  humannCommand = \"humann --diamond-options \\\" --block-size 0.8 --top 1 --outfmt 6\\\"
   functionalUnits = [\"level4ec\"]
 }
 
 process {
   executor = '$executor'
   queue = '$queue'
-  maxForks = 20
-  withLabel: 'mem_4c' {
-    errorStrategy = { task.exitStatus in 130..140 ? 'retry' : 'terminate' }
-    maxRetries = 3
-    clusterOptions = { task.attempt == 1 ?
-      '-n 4 -M 12000 -R \"rusage [mem=12000] span[hosts=1]\"'
-      : task.attempt == 2 ?
-      '-n 4 -M 17000 -R \"rusage [mem=17000] span[hosts=1]\"'
-      : '-n 4 -M 25000 -R \"rusage [mem=25000] span[hosts=1]\"'
+  maxForks = 40
+  withLabel: 'download_and_preprocess' {
+    errorStrategy {
+      sleep(Math.pow(2, task.attempt) * 500 as long);
+      if (task.exitStatus == 8 || task.attempt < 4 ) {
+        return 'retry'
+      } else {
+        return 'finish'
+      }
     }
+    maxRetries = 10
+    maxForks = 5
+    clusterOptions = '-M 4000 -R \"rusage [mem=4000] span[hosts=1]\"'
+  }
+  withName: 'runHumann' {
+    clusterOptions = '-M 8000 -R \"rusage [mem=8000] span[hosts=1]\"'
+    errorStrategy = 'finish'
   }
 }
 ";

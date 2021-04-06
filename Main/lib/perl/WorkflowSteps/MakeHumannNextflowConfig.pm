239f8b51865e8d8da38af63e794239436cecdd78
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-04-06 06:09:31 -0400

    Blocks of memory of 6GB in humann because that's how we're charged. Two retries, because sometimes we OOM due to a bit of uncertainty over usage in DIAMOND.

--- Main/lib/perl/WorkflowSteps/MakeHumannNextflowConfig.pm
+++ Main/lib/perl/WorkflowSteps/MakeHumannNextflowConfig.pm
@@ -36,7 +36,7 @@ sub run {
   resultDir = '$clusterResultDir'
   kneaddataCommand = \"kneaddata --trimmomatic ~/lib/Trimmomatic-0.39 --max-memory 3000m --bypass-trf --reference-db ~/kneaddata_databases\"
   wgetCommand = \"wget --waitretry=10 --read-timeout=20 --retry-connrefused --tries 3\"
-  humannCommand = \"humann --diamond-options \\\" --block-size 0.8 --top 1 --outfmt 6\\\"\"
+  humannCommand = \"humann --diamond-options \\\" --block-size 1.0 --top 1 --outfmt 6\\\"\"
   functionalUnits = [\"level4ec\"]
 }
 
@@ -46,7 +46,8 @@ process {
   maxForks = 40
   withLabel: 'download_and_preprocess' {
     errorStrategy = {
-      if (task.exitStatus == 8 || task.attempt < 4 ) {
+      sleep(Math.pow(2, task.attempt) * 500 as long);
+      if (task.exitStatus == 8 || task.exitStatus == 4 || task.attempt < 4 ) {
         return 'retry'
       } else {
         return 'finish'
@@ -54,15 +55,23 @@ process {
     }
     maxRetries = 10
     maxForks = 5
-    clusterOptions = '-M 4000 -R \"rusage [mem=4000] span[hosts=1]\"'
+    clusterOptions = {
+      (task.attempt > 1 && task.exitStatus in 130..140)
+        ? '-M 12000 -R \"rusage [mem=12000] span[hosts=1]\"'
+        : '-M 4000 -R \"rusage [mem=4000] span[hosts=1]\"'
+    }
   }
   withName: 'runHumann' {
-    clusterOptions = '-M 8000 -R \"rusage [mem=8000] span[hosts=1]\"'
-    errorStrategy = 'finish'
+    errorStrategy = { task.exitStatus in 130..140 ? 'retry' : 'finish' }
+    maxRetries = 2
+    clusterOptions = { task.attempt == 1 ?
+      '-M 12000 -R \"rusage [mem=12000] span[hosts=1]\"'
+      : '-M 18000 -R \"rusage [mem=18000] span[hosts=1]\"'
+    }
   }
 }
-";
 
+";
   close(F);
  }
 }

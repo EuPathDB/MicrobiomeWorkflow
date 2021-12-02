246ac7c2f443e312b6f285a61ea193d6a2bb5c7e
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-12-02 12:30:25 -0500

    Fix owl script

--- Main/scripts/extend_owl.pl
+++ Main/scripts/extend_owl.pl
@@ -6,12 +6,7 @@ use feature 'say';
 # pipe out to a temporary file
 # cut -f 1 /eupath/data/EuPathDB/workflows/MicrobiomeDB/5/data/all_results/*.eukdetect.lineage_abundance.tsv | sort -u
 
-my $owl = '
-    <owl:Class rdf:about="http://purl.obolibrary.org/obo/TMP_EUKDETECT_ORIGINAL">
-        <rdfs:subClassOf rdf:resource="http://purl.obolibrary.org/obo/OGMS_0000056"/>
-        <rdfs:label rdf:datatype="http://www.w3.org/2001/XMLSchema#string">EukDetect original result</rdfs:label>
-    </owl:Class>
-';
+my $owl = "";
 
 my $xml = "";
 
@@ -26,12 +21,16 @@ while(<>){
   @ls = reverse @ls;
   my $name = shift @ls;
   next unless $name;
-  my $p = parentTerm($kingdom);
   
-  my $id = "TMP_EUKDETECT_ORIGINAL_" . $c++; 
+  my $id1 = "TMP_EUKDETECT_M_" . $c++; 
   
-  $owl .= owlStanza($id, $name, parentTerm($kingdom));  
-  $xml .= xmlStanza($id, $name);
+  $owl .= owlStanza($id1, "$name detected", parentTerm($kingdom));  
+  $xml .= xmlStanza($id1, "$name detected");
+
+  my $id2 = "TMP_EUKDETECT_C_" . $c; 
+
+  $owl .= owlStanza($id2, "Matches for $name (CPM)", "EUPATH_0009269");  
+  $xml .= xmlStanza($id2, "Matches for $name (CPM)");
 }
 
 say $owl;
@@ -39,7 +38,6 @@ say $xml;
 
 sub parentTerm {
   my ($kingdom) = @_;
-  return "TMP_EUKDETECT_ORIGINAL";
   return $kingdom eq "Viridiplantae" ? 'EUPATH_0009264'
     : $kingdom eq "Metazoa" ? 'EUPATH_0009265'
     : $kingdom eq "Fungi" ? 'EUPATH_0009268'
@@ -52,7 +50,7 @@ sub owlStanza {
 
     <owl:Class rdf:about="http://purl.obolibrary.org/obo/$id">
         <rdfs:subClassOf rdf:resource="http://purl.obolibrary.org/obo/$parent"/>
-        <rdfs:label rdf:datatype="http://www.w3.org/2001/XMLSchema#string">$name originally detected</rdfs:label>
+        <rdfs:label rdf:datatype="http://www.w3.org/2001/XMLSchema#string">$name</rdfs:label>
     </owl:Class>
 EOF
   return $result;
@@ -61,8 +59,8 @@ sub xmlStanza {
   my ($id, $name) = @_;
 my $result = <<"EOF";
 
-  <ontologyTerm source_id="$id" type="characteristicQualifier" parent="Assay">
-    <name>Original:$name</name>
+  <ontologyTerm source_id="$id" type="characteristicQualifier" parent="Source">
+    <name>$name</name>
   </ontologyTerm>
 EOF
   return $result;

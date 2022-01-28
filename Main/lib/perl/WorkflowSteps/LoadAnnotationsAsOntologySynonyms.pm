523fe5f25c670062d3694a36623facc27764bc9b
Wojtek Bazant <wbazant@vet.upenn.edu>
2022-01-28 12:15:06 -0500

    New step adds synonyms

---
+++ Main/lib/perl/WorkflowSteps/LoadAnnotationsAsOntologySynonyms.pm
@@ -0,0 +1,27 @@
+package MicrobiomeWorkflow::Main::WorkflowSteps::LoadAnnotationsAsOntologySynonyms;
+
+# inspired by
+# package ApiCommonWorkflow::Main::WorkflowSteps::UpdateOntologySynonym
+
+
+@ISA = (ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep);
+
+use strict;
+use warnings;
+
+use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
+
+
+sub run {
+    my ($self, $test, $undo) = @_;
+
+    my @args = ();
+    push @args, "--attributesFile", join ("/", $self->getWorkflowDataDir(), $self->getParamValue("attributesFile"));
+    push @args, "--extDbRlsSpec", $self->getParamValue('extDbRlsSpec');
+  
+    $self->runPlugin($test, $undo, "GUS::Supported::Plugin::InsertOntologySynonymAttributes", join(" ", @args));
+}
+
+1;
+
+

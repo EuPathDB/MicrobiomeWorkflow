77cbd7dd5f4ab3bc9a1f983f96b51b354a4a46c3
Jay Humphrey <jaycolin@apidb.org>
2022-10-18 10:46:23 -0400

    Add ontologyMapping.xml and valueMap.txt, force for now, later we will assign these params

--- Main/lib/perl/WorkflowSteps/MBioInsertEntityGraph.pm
+++ Main/lib/perl/WorkflowSteps/MBioInsertEntityGraph.pm
@@ -4,15 +4,20 @@ package MicrobiomeWorkflow::Main::WorkflowSteps::MBioInsertEntityGraph;
 
 use strict;
 use warnings;
+use File::Basename;
 use ApiCommonWorkflow::Main::WorkflowSteps::WorkflowStep;
 
 sub run {
     my ($self, $test, $undo) = @_;
 
+    my $mbioResultsDir = join ("/", $self->getWorkflowDataDir(), $self->getParamValue("mbioResultsDir"));
+    my $datasetDir = dirname($mbioResultsDir);
     my @args = ();
     push @args, "--investigationFile", join ("/", $self->getWorkflowDataDir(), $self->getParamValue("investigationFile"));
     push @args, "--sampleDetailsFile", join ("/", $self->getWorkflowDataDir(), $self->getParamValue("sampleDetailsFile"));
     push @args, "--ontologyMappingFile", join ("/", $self->getWorkflowDataDir(), $self->getParamValue("ontologyMappingFile"));
+    push @args, "--ontologyMappingOverrideFile", join ("/", $datasetDir, "final", "ontologyMapping.xml");
+    push @args, "--valueMappingFile", join ("/",  $datasetDir, "final", "valueMap.txt");
     push @args, "--mbioResultsDir", join ("/", $self->getWorkflowDataDir(), $self->getParamValue("mbioResultsDir"));
     push @args, "--mbioResultsFileExtensions", $self->getParamValue("mbioResultsFileExtensions");
     push @args, "--extDbRlsSpec", "'" . $self->getParamValue("extDbRlsSpec") . "'";
